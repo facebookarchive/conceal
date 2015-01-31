7a3b325dc4be1687fecce675b46514be0e9df06f
Subodh Iyengar <subodh@fb.com>
2015-01-30 17:42:21 -0800

    Upgrade openssl to 1.0.2
    Summary:
    Upgrade libcrypto to the 1.0.2
    version 4ac0329582829f5378d8078c8d314ad37db87736.
    
    This is a slight increase in binary
    size over the previous version, since
    I have tried to keep source code
    modifications to a minimum.
    
    We're also changing the way we're managing
    openssl. We're going to start checking in
    openssl as prebuilt binaries instead of
    checking in the source and we'll still post
    patches so that you can build your own
    openssl with the magic parameter incantation.
    
    This change will allow us to more quickly
    respond to openssl upgrades so that we can
    keep up-to-date.
    
    Test Plan:
    Ran tests on armeabi devices, armeabi-v7a devices
    as well as an x86 emulator, they all work as
    expected.

--- native/Application.mk
+++ native/Application.mk
@@ -1,4 +1,4 @@
 # Copyright 2004-present Facebook. All Rights Reserved.
 
-APP_ABI := armeabi-v7a armeabi x86
+APP_ABI := armeabi armeabi-v7a x86
 NDK_TOOLCHAIN_VERSION := 4.8
--- native/crypto/Android.mk
+++ native/crypto/Android.mk
@@ -3,11 +3,11 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := conceal
-LOCAL_CFLAGS    := -Werror
+LOCAL_CFLAGS    := -fvisibility=hidden -Os
 LOCAL_SRC_FILES := gcm.c gcm_util.c hmac.c hmac_util.c init.c util.c
 LOCAL_LDLIBS    := -llog
 
-LOCAL_SHARED_LIBRARIES += libcryptox
+LOCAL_SHARED_LIBRARIES += crypto
 include $(BUILD_SHARED_LIBRARY)
 
-$(call import-module,cryptox)
+$(call import-module,openssl)
