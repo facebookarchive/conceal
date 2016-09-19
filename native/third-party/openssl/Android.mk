fd6402f3da6d2da48fe8d5f25dd0efa9eda687ed
Helios Alonso <heliosalonso@fb.com>
2016-09-19 16:37:18 -0700

    Compile options to include only used functions from OpenSSL

--- native/third-party/openssl/Android.mk
+++ native/third-party/openssl/Android.mk
@@ -1,9 +1,10 @@
 LOCAL_PATH := $(call my-dir)
 
-local_c_includes := $(LOCAL_PATH)/include 
+local_c_includes := $(LOCAL_PATH)/include
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := crypto
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libcrypto.a
+LOCAL_CFLAGS    := -fdata-sections -ffunction-sections
 LOCAL_EXPORT_C_INCLUDES := $(local_c_includes)
 include $(PREBUILT_STATIC_LIBRARY)
